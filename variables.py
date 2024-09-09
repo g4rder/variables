fd61e4b746ce48b82b79e9976550227e592154e2
g4rder <ilyagarder@mail.ru>
2024-09-09 22:07:56 +0700

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,5 @@
+complited_assigments = 12   # кол-во выполненых ДЗ
+hours_spent = 1.5           # кол-во затраченного времени
+course_name = 'Python'      # название курса
+time_per_assigment = hours_spent / complited_assigments # время на задание
+print('Курс:' + course_name + ', всего задач: ' + str(complited_assigments) + ', затрачено часов: ' + str(hours_spent) + ', среднее время выполнения: ', str(time_per_assigment) + '.')
